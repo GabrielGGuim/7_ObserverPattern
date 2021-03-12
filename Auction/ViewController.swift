02790cd4b4e1fab3fe4aa4c90cd5d2e365c6f0ff
Gabriel Gonçalves <GGGguimaraesgabriel@hotmail.com>
2021-03-11 23:59:12 -0300

    Add Layout in view

---
+++ Auction/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Auction
+//
+//  Created by Gabriel Gonçalves Guimarães on 09/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let uiLayout = viewUI()
+    
+    override func loadView() {
+        
+        self.view = uiLayout
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
